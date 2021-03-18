0a974c365f4738124366102b7283f794f0d62027
Jon Kartago Lamida <me@lamida.net>
2021-03-19 00:02:40 +0800

    Rewritten the simulator

--- swat_rwt.py
+++ swat_rwt.py
@@ -1,56 +1,214 @@
 import sys
 import time
+from enum import Enum
 
-T101_H = 800  # mm
-T101_L = 500  # mm
-P101 = 1  # ON
-MV101 = 0  # OPEN
-INFLOW_OPEN_ON = 1.11857  # mm/min
-INFLOW_OPEN_OFF = 28.5234  # mm/min
-OUTFLOW_CLOSED_ON = 27.40  # mm/min
-PLC_SAMPLES = 1000
-PLC_PERIOD_SEC = 0.10
 
-count = 0
-new_level = 500
-while(count <= PLC_SAMPLES):
+"""
+State of a valve.
+"""
 
-    new_level = new_level
-    #print(str(MV101),str(P101))
 
-    # inflows volumes
-    if str(MV101) == "0" and str(P101) == "0":
-        break
+class StateValve(Enum):
+    CLOSE = 0
+    OPEN = 1
 
-    elif str(MV101) == "1" and str(P101) == "1":
-        new_level += INFLOW_OPEN_ON
 
-    elif str(MV101) == "1" and str(P101) == "0":
-        new_level += INFLOW_OPEN_OFF
+"""
+Valve allows water flow when it opens and stop the flow when it closes.
+"""
 
-    else:
-        new_level -= OUTFLOW_CLOSED_ON
 
-    # level cannot be negative
-    if new_level <= 0.0:
-        new_level = 0.0
+class Valve:
 
-    # HIGHER from 0.800 m
-    if new_level >= T101_H:
-        print('DEBUG RawWaterTank T101 above H count: ', count)
-        MV101 = 0
-        P101 = 1
+    """
+    Create a valve with a name and initial state. 
+    """
 
-    # LOWER T 0.500 m
-    elif new_level <= T101_L:
-        print('DEBUG RawWaterTank T101 below L count: ', count)
-        MV101 = 1
-        P101 = 0
+    def __init__(self, name: str, state: StateValve = StateValve.CLOSE):
+        self.name = name
+        self.state = state
 
-    elif new_level < T101_H and new_level > T101_L:
-        MV101 = 1
-        P101 = 1
+    """
+    Check whether the valve is open.
+    """
 
-    count += 1
-    print('water level ', new_level, ' @ count ', count)
-    time.sleep(PLC_PERIOD_SEC)
+    def is_open(self) -> bool:
+        return self.state == StateValve.OPEN
+
+    """
+    Check whether the valve is close.
+    """
+
+    def is_close(self) -> bool:
+        return not self.is_open()
+
+    def __change_state(self, new_state: StateValve):
+        if new_state != self.state:
+            print("Change Valve state", self.name, "to", new_state)
+            self.state = new_state
+
+    """
+    Open the valve.
+    """
+
+    def open(self):
+        self.__change_state(StateValve.OPEN)
+
+    """
+    Close the valve.
+    """
+
+    def close(self):
+        self.__change_state(StateValve.CLOSE)
+
+
+"""
+State of a pump.
+"""
+
+
+class StatePump(Enum):
+    OFF = 0
+    ON = 1
+
+
+"""
+Pump will suck the water out from a tank when it is on and will do nothing when it is off.
+"""
+
+
+class Pump:
+
+    """
+    Create a pump using a name and an initial state.
+    """
+
+    def __init__(self, name: str, state: StatePump = StatePump.OFF):
+        self.name = name
+        self.state = state
+
+    """
+    Check whether the pump is on.
+    """
+
+    def is_on(self) -> bool:
+        return self.state == StatePump.ON
+
+    """
+    Check whether the pump is off.
+    """
+
+    def is_off(self) -> bool:
+        return not self.is_on()
+
+    def __change_state(self, new_state: StatePump):
+        if new_state != self.state:
+            print("Change Pump state", self.name, "to", new_state)
+            self.state = new_state
+
+    """
+    Turn on the pump.
+    """
+
+    def turn_on(self):
+        self.__change_state(StatePump.ON)
+
+    """
+    Turn off the pump.
+    """
+
+    def turn_off(self):
+        self.__change_state(StatePump.ON)
+
+
+"""
+Tank is a water container. It maintain a water level. It connects to an inlet valve and sink to a pump out. 
+The water level in a tank must never goes to zero.
+"""
+
+
+class Tank:
+    """
+    Create a tank.
+    """
+
+    def __init__(self, name: str, current_level: float, high: float, low: float, inlet_valve: Valve, pump_out: Pump):
+        self.name = name
+        self.current_level = current_level
+        self.high = high
+        self.low = low
+        self.inlet_valve = inlet_valve
+        self.pump_out = pump_out
+
+    """
+    Return water change rate in the tank.
+
+    If it is positive it means water level is rising.
+    If it is negative it means water level is falling.
+
+    speed_factor can be set to speed up the simulation. speed_factor = 1 means the water_level change
+    rate will be in real actual time. speed_factor = x will speed up the simulation to the x factor.
+    """
+
+    def water_level_rate(self, speed_factor: float = 1) -> float:
+        seconds_in_minute = 60
+        if self.inlet_valve.is_open() and self.pump_out.is_on():
+            return speed_factor * 1.11857 / seconds_in_minute
+        elif self.inlet_valve.is_open() and self.pump_out.is_off():
+            return speed_factor * 28.5234 / seconds_in_minute
+        elif self.inlet_valve.is_close() and self.pump_out.is_on():
+            return speed_factor * -27.40 / seconds_in_minute
+        else:
+            return 0
+
+    """
+    Update the state of tank. A speed_factor can be passed to speed up the simulation.
+    """
+
+    def update_state(self, speed_factor: float):
+        new_level = self.current_level + self.water_level_rate(speed_factor)
+        if new_level >= 0:
+            self.current_level = new_level
+        else:
+            self.current_level = 0
+
+        if new_level > self.high:
+            print("Warning! The water is overflowing!")
+
+        if self.current_level <= self.low:
+            self.inlet_valve.open()
+            self.pump_out.turn_off()
+
+        if self.current_level >= self.high:
+            self.inlet_valve.close()
+            self.pump_out.turn_on()
+
+        if self.current_level > self.low and self.current_level < self.high:
+            self.inlet_valve.open()
+            self.pump_out.turn_on()
+
+
+class Simulator:
+    def __init__(self, tick_time: float = 1, speed_factor: float = 1):
+        self.p101 = Pump("P101")
+        self.mv101 = Valve("MV101")
+        self.t101 = Tank(name="T101", current_level=600, low=500,
+                         high=800, inlet_valve=self.mv101, pump_out=self.p101)
+        self.tick_time = tick_time
+        self.speed_factor = speed_factor
+
+    def run(self):
+        print("=========================================================================================")
+        print(f"Running simulator with tick time: {self.tick_time} and speed factor: {self.speed_factor}") 
+        print("Press CTRL + C or CTRL + Z to stop")
+        print("=========================================================================================")
+        print()
+        while(True):
+            self.t101.update_state(self.speed_factor)
+            print("Tank water level:", self.t101.current_level)
+            time.sleep(self.tick_time)
+
+
+if __name__ == "__main__":
+    simulator = Simulator(tick_time=1, speed_factor=500)
+    simulator.run()
